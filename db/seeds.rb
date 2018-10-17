e6ef0900c326b57be154588ea68249e4e5026938
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2018-10-17 17:46:21 +0200

    exo done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,85 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'East Mamma',
+    address:      'Bastille',
+    phone_number: '01 45 00 00 00',
+    category:     'italian'
+  },
+  {
+    name:         'Ober Mamma',
+    address:      'Oberkampf',
+    phone_number: '01 45 00 00 00',
+    category:     'italian'
+  },
+  {
+    name:         'Mamma Primi',
+    address:      '17ème',
+    phone_number: '01 45 00 00 00',
+    category:     'italian'
+  },
+  {
+    name:         'Big Love',
+    address:      '12 rue Emile Dubois',
+    phone_number: '01 45 00 00 00',
+    category:     'italian'
+  },
+  {
+    name:         'Popolare',
+    address:      'Sentier',
+    phone_number: '01 45 00 00 00',
+    category:     'italian'
+  },
+  {
+    name:         'Pink Mamma',
+    address:      'Nord',
+    phone_number: '01 45 00 00 00',
+    category:     'italian'
+  },
+  {
+    name:         'Kisoro',
+    address:      'Lille',
+    phone_number: '01 45 00 00 00',
+    category:     'japanese'
+  },
+  {
+    name:         'Pistolet',
+    address:      'Reaumur',
+    phone_number: '01 45 00 00 00',
+    category:     'belgian'
+  },
+  {
+    name:         'Orient Extreme',
+    address:      'Neuilly',
+    phone_number: '01 45 00 00 00',
+    category:     'chinese'
+  },
+  {
+    name:         'L\'entrecôte',
+    address:      'Porte Maillot',
+    phone_number: '01 45 00 00 00',
+    category:     'french'
+  },
+  {
+    name:         'Brasserie Auteuil',
+    address:      'Porte d\'auteuil',
+    phone_number: '01 45 00 00 00',
+    category:     'french'
+  },
+  {
+    name:         'Dôme',
+    address:      'Villiers',
+    phone_number: '01 45 00 00 00',
+    category:     'french'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
